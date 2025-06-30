72da1e8f5b3132e851de0f4b4d5ce429fbf330d6
Salla Harish <sallaharish040@gmail.com>
2025-06-30 09:48:03 +0530

    added the header and fooert and necessary packagesf or the project setup

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./NavBar";
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />} > 
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      {/* <NavBar /> */}
+    </>
+  );
+}
+
+export default App;
